9b3a54cf223d1915362b84c4d87143c99c699288
Shtajnbah <eshtacznbax@bk.ru>
2022-02-21 18:58:25 +0300

    вырвнивание и столбцы

--- lib/main.dart
+++ lib/main.dart
@@ -6,15 +6,31 @@ void main() {
           appBar: AppBar(
             title: Text('Hello world!', style: TextStyle(fontSize: 32)),
           ),
-          body: Align(
-            alignment: Alignment(0, -1),
-            child: Text(
-              'Hello world!',
-              style: TextStyle(
-                  fontSize: 32,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.indigo),
-            ),
+
+          body: Container(
+            decoration: BoxDecoration(
+                color: Colors.teal, borderRadius: BorderRadius.circular(3),),
+            /*width: 300,
+              height: 300,
+              padding: EdgeInsets.only(left: 30, top: 50),//внутренний
+              margin: EdgeInsets.only(left: 30, top: 50),//внешний
+              alignment: Alignment.center,//выравнивание*/
+            child:Column(
+             // mainAxisAlignment: MainAxisAlignment.center, //вырвнивание по центру
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              crossAxisAlignment: CrossAxisAlignment.start,//по веритикали, не основной оси
+
+              children:  <Widget>[
+                Container(
+                  decoration: BoxDecoration(color: Colors.white),
+                  padding: EdgeInsets.all(10),
+                  child:Icon(Icons.all_inclusive_rounded, size: 50, color: Colors.amber,),
+                ),
+              Icon(Icons.access_time_rounded, size: 50, color: Colors.amber,),
+              Icon(Icons.ac_unit, size: 150, color: Colors.lightBlueAccent,),
+              Icon(Icons.account_circle_outlined, size: 80, color: Colors.deepPurple,),
+
+              ],),
           )
       )));
 }
