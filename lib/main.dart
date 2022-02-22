6970f86cc9aaa927ae1a9db99d531208d7c12251
Shtajnbah <eshtacznbax@bk.ru>
2022-02-22 10:57:42 +0300

    stack

--- lib/main.dart
+++ lib/main.dart
@@ -7,30 +7,37 @@ void main() {
             title: Text('Hello world!', style: TextStyle(fontSize: 32)),
           ),
 
-          body: Container(
-            decoration: BoxDecoration(
-                color: Colors.teal, borderRadius: BorderRadius.circular(3),),
-            /*width: 300,
-              height: 300,
-              padding: EdgeInsets.only(left: 30, top: 50),//внутренний
-              margin: EdgeInsets.only(left: 30, top: 50),//внешний
-              alignment: Alignment.center,//выравнивание*/
-            child:Column(
-             // mainAxisAlignment: MainAxisAlignment.center, //вырвнивание по центру
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              crossAxisAlignment: CrossAxisAlignment.start,//по веритикали, не основной оси
+          body: Stack(
+              alignment: Alignment.center,
+              //выравнивание относительно друг друга
+              //последний объект выше остальных
+              children: [
+                /*Positioned(
+                  top:10,
+                  left:10
+                  child: Text(
+                    'Stack',
+                    style: TextStyle(fontSize: 30),*/
 
-              children:  <Widget>[
+                /* Text(
+                'Stack',
+                style: TextStyle(fontSize: 30),
+                ),*/
                 Container(
-                  decoration: BoxDecoration(color: Colors.white),
-                  padding: EdgeInsets.all(10),
-                  child:Icon(Icons.all_inclusive_rounded, size: 50, color: Colors.amber,),
+                  width: 200,
+                  decoration: BoxDecoration(color: Colors.black),
                 ),
-              Icon(Icons.access_time_rounded, size: 50, color: Colors.amber,),
-              Icon(Icons.ac_unit, size: 150, color: Colors.lightBlueAccent,),
-              Icon(Icons.account_circle_outlined, size: 80, color: Colors.deepPurple,),
-
-              ],),
+                Icon(
+                  Icons.airplay,
+                  color: Colors.lightBlueAccent,
+                  size: 120,
+                ),
+                Icon(
+                  Icons.favorite_border_outlined,
+                  color: Colors.red,
+                  size: 400,
+                )
+              ]
           )
       )));
 }
